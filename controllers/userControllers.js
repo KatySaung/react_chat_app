56d6c005c6d4ae2738e0868228e538c680d66d94
Katherine Yanan Saung <katysaung@gmail.com>
2024-03-27 19:42:07 -0400

    created and tested in postman all routes and controllers for user

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,8 +1,8 @@
 import expressAsyncHandler from "express-async-handler";
+export { registerUser, login, logout, updateUser, deleteUser };
 
-// Register User
-// route  POST /api/users/register
-// access Public
+// route    POST /api/user/register
+// access   Public
 const registerUser = expressAsyncHandler(async (req, res) => {
   // res.status(401);
   // throw new Error("Ohh Ohh 401 unauthorized");
@@ -10,4 +10,29 @@ const registerUser = expressAsyncHandler(async (req, res) => {
   res.status(200).json({ message: "Registered User" });
 });
 
-export { registerUser };
+// route  POST /api/user/auth
+// JWT    Private
+const login = expressAsyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Authorized User" });
+});
+
+// route  POST /api/user/logout
+// JWT    Private
+const logout = expressAsyncHandler(async (req, res) => {
+  // res.status(401);
+  // throw new Error("Ohh Ohh 401 unauthorized");
+
+  res.status(200).json({ message: "Logged Out User" });
+});
+
+// route    PUT /api/user/profile
+// JWT      Private
+const updateUser = expressAsyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Updated User" });
+});
+
+// route    DELETE /api/user/profile
+// JWT      Private
+const deleteUser = expressAsyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Deleted User" });
+});
